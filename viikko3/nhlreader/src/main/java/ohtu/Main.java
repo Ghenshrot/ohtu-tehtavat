fb6524f460f01d81ce666c6eda2b48383bfe80c6
Joni Yrjänä <joni.yrjana@helsinki.fi>
2019-11-16 13:12:17 +0200

    Week3-1: List players by nationality.

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
     
+    private static List<Player> all_players;
+    
+    public static void main(String[] args) throws IOException {
+        readPlayerData();
+        
+        String country = "FIN";
+        System.out.println("Players from " + country + ":");
+        getPlayersByNationality(country).forEach(p -> {
+            System.out.println(p);
+        });
+    }
+    
+    public static void readPlayerData() throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+        String player_data = Request.Get(url).execute().returnContent().asString();
+
+        Gson mapper = new Gson();
+        Type t = new TypeToken<Collection<Player>>() {}.getType();
+        all_players = mapper.fromJson(player_data, t);
+    }
+    
+    public static List<Player> getPlayersByNationality(String nationality) {
+        return all_players
+                .stream()
+                .filter(p -> p.getNationality().equals(nationality))
+                .collect(Collectors.toList());
+    }
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,6 +3,10 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private String team;
+    private int    goals;
+    private int    assists;
 
     public void setName(String name) {
         this.name = name;
@@ -11,10 +15,30 @@ public class Player {
     public String getName() {
         return name;
     }
+    
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    public String getNationality() {
+        return nationality;
+    }
+    
+    public void setTeam(String team) {
+        this.team = team;
+    }
+    
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+    
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
 
     @Override
     public String toString() {
-        return name;
+        return name + " team " + team + " goals " + goals + " assists " + assists;
     }
       
 }
