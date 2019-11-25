c5b972287cddc06ee4a24bff1fd77eeefb941f28
Joni Yrjänä <joni.yrjana@helsinki.fi>
2019-11-25 21:29:02 +0200

    Week4-6: Refactor TennisGame for better readability.

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,80 +1,74 @@
 package ohtu;
 
 public class TennisGame {
+    private int player1Points = 0;
+    private int player2Points = 0;
+    private final String player1Name;
+    private final String player2Name;
+    // The possible scores before "advantage":
+    private final String[] scoreNames = { "Love", "Fifteen", "Thirty", "Forty" };
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
 
+    
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            player1Points += 1;
+        } else {
+            player2Points += 1;
+        }
     }
 
+    
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        String score = checkForEqualScores();
+        if (score != null) {
+            return score;
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        score = checkForAdvantageAndWin();
+        if (score != null) {
+            return score;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return getScoreName(player1Points) + "-" + getScoreName(player2Points);
+    }
+
+    
+    private String checkForEqualScores() {
+        if (player1Points != player2Points) {
+            return null;
         }
-        return score;
+        if (player1Points >= scoreNames.length) {
+            return "Deuce";
+        }
+        return getScoreName(player1Points) + "-All";
+    }
+
+    
+    private String checkForAdvantageAndWin() {
+        if (!isAdvantage(player1Points) && !isAdvantage(player2Points)) {
+            return null;
+        }
+
+        int pointsDiff = player1Points - player2Points;
+        String winningPlayer = pointsDiff > 0 ? player1Name : player2Name;
+
+        if (Math.abs(pointsDiff) == 1) {
+            return "Advantage " + winningPlayer;
+        }
+        return "Win for " + winningPlayer;
+    }
+    
+
+    private String getScoreName(int score) {
+        return scoreNames[score];
+    }
+    
+
+    private boolean isAdvantage(int points) {
+        return points >= scoreNames.length;
     }
 }
