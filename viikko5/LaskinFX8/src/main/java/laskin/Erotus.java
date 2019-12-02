5a5756b736906a6a8f84213724b58265dd599ba1
Joni Yrjänä <joni.yrjana@helsinki.fi>
2019-12-02 14:24:24 +0200

    Week4-4: Initial refactoring of operations to Komento class.

---
+++ viikko5/LaskinFX8/src/main/java/laskin/Erotus.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Joni Yrjänä <joni.yrjana@helsinki.fi>
+ */
+public class Erotus extends Komento {
+
+    public Erotus(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        getSovellus().miinus(getLuku2());
+        tulos();
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ viikko5/LaskinFX8/src/main/java/laskin/Komento.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+/**
+ *
+ * @author Joni Yrjänä <joni.yrjana@helsinki.fi>
+ */
+public abstract class Komento {
+    private TextField        tuloskentta;
+    private TextField        syotekentta;
+    private Button           nollaa;
+    private Button           undo;
+    private Sovelluslogiikka sovellus;
+    
+    public Komento(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa      = nollaa;
+        this.undo        = undo;
+        this.sovellus    = sovellus;
+    }
+    
+    public abstract void suorita();
+    public abstract void peru();
+    
+    protected Sovelluslogiikka getSovellus() {
+        return sovellus;
+    }
+    
+    private int getLuku(TextField kentta) {
+        int arvo = 0;
+        try {
+            arvo = Integer.parseInt(kentta.getText());
+        } catch (Exception e) {
+        }
+        return arvo;
+    }
+    
+    // todo: refactor to somewhere else:
+    protected int getLuku1() {
+        return getLuku(tuloskentta);
+    }
+    
+    protected int getLuku2() {
+        return getLuku(syotekentta);
+    }
+    
+    protected void tulos() {
+        int laskunTulos = sovellus.tulos();
+        
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+        
+        if ( laskunTulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+}
